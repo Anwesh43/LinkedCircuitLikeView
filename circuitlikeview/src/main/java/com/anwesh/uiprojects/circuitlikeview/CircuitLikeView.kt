2a060c23516cde30757da20eebe0dbd39c3207ee
Anwesh43 <anweshthecool0@gmail.com>
2019-07-24 11:28:34 +0530

    adding CLNode class

--- circuitlikeview/src/main/java/com/anwesh/uiprojects/circuitlikeview/CircuitLikeView.kt
+++ circuitlikeview/src/main/java/com/anwesh/uiprojects/circuitlikeview/CircuitLikeView.kt
@@ -132,4 +132,48 @@ class CircuitLikeView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CLNode(var i : Int, val state : State = State()) {
+
+        private var next : CLNode? = null
+        private var prev : CLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CLNode {
+            var curr : CLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
